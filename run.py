7ff1d12839dd893c670ca2c57277a1cbd76951f7
Rutherther <rutherther@proton.me>
2024-01-06 20:24:58 +0100

    feat: add libraries to vunit

--- run.py
+++ run.py
@@ -13,11 +13,20 @@ i2c_tb_lib.add_source_files(Path(__file__).parent / 'tb/i2c/**/*.vhd')
 mcu_slave_tb_lib = vu.add_library('mcu_slave_tb')
 mcu_slave_tb_lib.add_source_files(Path(__file__).parent / 'tb/mcu_slave/**/*.vhd')
 
-utils_lib = vu.add_library('mcu_slave')
-utils_lib.add_source_files(Path(__file__).parent / 'src/mcu_slave/**/*.vhd')
+mcu_slave_lib = vu.add_library('mcu_slave')
+mcu_slave_lib.add_source_files(Path(__file__).parent / 'src/mcu_slave/**/*.vhd')
+
+ssd1306_lib = vu.add_library('ssd1306')
+ssd1306_lib.add_source_files(Path(__file__).parent / 'src/ssd1306/**/*.vhd')
+
+ssd1306_lib = vu.add_library('ssd1306_tb')
+ssd1306_lib.add_source_files(Path(__file__).parent / 'tb/ssd1306/**/*.vhd')
 
 utils_lib = vu.add_library('utils')
 utils_lib.add_source_files(Path(__file__).parent / 'src/utils/**/*.vhd')
+
+utils_tb_lib = vu.add_library('utils_tb')
+utils_tb_lib.add_source_files(Path(__file__).parent / 'tb/utils/**/*.vhd')
 
 i2c_lib = vu.add_library('i2c')
 i2c_lib.add_source_files(Path(__file__).parent / 'src/**/*.vhd')
